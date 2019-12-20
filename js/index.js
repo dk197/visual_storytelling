4432c0883ec63a2f4d910dbd6ceeb017672fa76a
liltschou <38752135+liltschou@users.noreply.github.com>
2019-12-20 16:24:01 +0100

    adding counting numbers

--- js/index.js
+++ js/index.js
@@ -116,3 +116,17 @@ function getDataFromCsv(filename) {
     //     }
     //  });
 }
+
+window.onload = function() {
+    $('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1500,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+}
